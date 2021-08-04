0320b917f33466a5346e4601d5c4ee81a45ceee5
ONrime <ekwnddl20@gmail.com>
2021-08-04 21:19:00 +0900

    부가 설명 추가

--- Source/ProneSystem/Character_AnimInstance.cpp
+++ Source/ProneSystem/Character_AnimInstance.cpp
@@ -18,48 +18,56 @@ void UCharacter_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 		AProneSystemCharacter* Player = Cast<AProneSystemCharacter>(Pawn);
 
 		FRotator PitchAngle = (Player->GetControlRotation() - Player->GetActorRotation()).GetNormalized();
+		// 컨트롤러와 액터 사이의 pitch 값을 구한다.
 		UpperPitch = FMath::ClampAngle(PitchAngle.Pitch, -90.0f, 90.0f);
+		// 허리를 180도로 꺽는게 아니기 때문에 90도로 제한을 둔다.
 
-		IsProne = Player->GetIsProne();
+		PlayerSpeed = Player->GetVelocity().Size(); // 플레이어 속도
+		IsProne = Player->GetIsProne(); // 엎드리기 상태 구분
 
-		if (IsProne) {
-			if (Player->GetVelocity().Size() > 3.0f)
-			{ // 움직일 때 하체가 따라가게
+		if (IsProne) // 플레이어가 엎드린다면
+		{ 
+			if (PlayerSpeed > 3.0f)
+			{ 
+				// 움직일 때 고정된 방향(TurnDirEnd)을 움직이는 방향으로 바꾼다.
 				TurnDirEnd = Player->GetActorRotation();
+
 				if (!(UpperProneYaw <= 110.0f && UpperProneYaw >= -110.0f))
-				{
+				{ 
+					// 만약 등이 바닥을 향하게 누워있다면 향해야 되는 방향을 반대로 하기
 					TurnDirEnd = FRotator(TurnDirEnd.Pitch, TurnDirEnd.Yaw + 180.0f, TurnDirEnd.Roll);
-					IsProneBack = true;
+					IsProneBack = true; // 누워있는 상태
 				}
 				else {
-					IsProneBack = false;
+					IsProneBack = false;  // 엎드려있는 상태
 				}
 			}
 			else
-			{ // 하체 따로
-				if (!IsTurn)
+			{ 
+				// 움직이지 않을 때 TurnDirEnd을 ActorRotation으로 고정 시킨다.
+				if (!IsTurn) // 시작되는 순간에만 고정 되게끔 해야 된다.
 				{
 					IsTurn = true;
 					TurnDirEnd = Player->GetActorRotation();
 				}
 				if (!(UpperProneYaw <= 110.0f && UpperProneYaw >= -110.0f))
 				{
-					IsProneBack = true;
+					IsProneBack = true; // 누워있는 상태
 				}
 				else {
-					IsProneBack = false;
+					IsProneBack = false; // 엎드려있는 상태
 				}
 			}
 
 		}
 		else {
-			if (Player->GetVelocity().Size() > 3.0f)
-			{ // 움직일 때 하체가 따라가게
+			if (PlayerSpeed > 3.0f)
+			{ // 움직일 때 하체가 따라가게한다.
 				TurnDirEnd = Player->GetActorRotation();
 				IsTurn = false;
 			}
 			else
-			{ // 하체 따로
+			{ // 움직이지 않을 때 하체가 따로 움직이게 한다.
 				if (!IsTurn)
 				{
 					IsTurn = true;
@@ -73,11 +81,12 @@ void UCharacter_AnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 		}
 		TurnDir = FMath::RInterpTo(TurnDir, TurnDirEnd, GetWorld()->GetDeltaSeconds(), 5.0f);
 		FRotator YawToAngle = (Player->GetActorRotation() - TurnDir).GetNormalized();
-		UpperProneYaw = YawToAngle.Yaw;
+		UpperProneYaw = YawToAngle.Yaw; // 엎드리기 상태일 때 는 전체를 회전 함으로 180도 값을 받아야 한다.
 		UpperYaw = FMath::ClampAngle(YawToAngle.Yaw, -90.0f, 90.0f);
 		//UE_LOG(LogTemp, Warning, TEXT("UpperYaw: %f"), UpperYaw);
 		//UE_LOG(LogTemp, Warning, TEXT("UpperProneYaw: %f"), UpperProneYaw);
 
+		// 엎드리기 상태 일 때 위 아래를 바라보면 그에 따라 상체가 움직일 수 있게 하는 값이다.
 		ProneRot = GetProneRotBlend(TurnDirEnd.Vector(), Player->GetActorRotation());
 	}
 	
